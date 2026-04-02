8dba97bbef0f5dee02eaa14a177082df6669bca6
David Turner <davidt0x@gmail.com>
2026-04-02 14:20:40 -0400

    fixes for running on rig

--- run.m
+++ run.m
@@ -8,23 +8,23 @@ roiCircles = [
 ];
 
 % --- WINVIDEO (e.g., generic USB camera)
-vid = roi_stream('linuxvideo', 1, '', roiCircles);  % auto-picks device & format
+%vid = roi_stream('linuxvideo', 1, '', roiCircles);  % auto-picks device & format
 %vid = roi_stream('winvideo', 1, '', roiCircles);  % auto-picks device & format
 %vid = roi_stream('winvideo', 2, 'I420_1280x720', roiCircles);  % auto-picks device & format
 
 
 % --- or HAMAMATSU (DCAM adaptor)
-% vid = roi_stream('hamamatsu', [], '', roiCircles);
+vid = roi_stream('hamamatsu', [], '', roiCircles);
 
 % 3) Launch the GUI (updates every second, plots last 60 s)
 h = roi_stream_gui(vid, struct('PlotWindowSec',60, 'UpdatePeriod',1.0));
 
 %%
-run_random_rois
+%run_random_rois
 
 %%
 % Let it run, then stop:
 trace_file_name = stop_roi_stream(vid);
 
 %%
-h5_traces_viewer(trace_file_name)
+%h5_traces_viewer(trace_file_name)
